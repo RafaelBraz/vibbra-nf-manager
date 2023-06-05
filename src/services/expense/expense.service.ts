600c7a4a67238b3b28d6b07f590189404bcd9fd9
Rafael Braz <rafaelvcbraz@gmail.com>
2023-06-05 10:28:00 -0300

    ExpenseService

---
+++ src/services/expense/expense.service.ts
@@ -0,0 +1,46 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+
+export class ExpenseService {
+  private prisma: PrismaClient;
+
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+
+  async findUnique(where: Prisma.ExpenseWhereUniqueInput) {
+    return this.prisma.expense.findUnique({
+      where,
+    });
+  }
+
+  async findMany(params: { where?: Prisma.ExpenseWhereInput }) {
+    const { where } = params;
+
+    return this.prisma.expense.findMany({
+      where,
+    });
+  }
+
+  async update(params: {
+    where: Prisma.ExpenseWhereUniqueInput;
+    data: Prisma.ExpenseUpdateInput;
+  }) {
+    const { where, data } = params;
+    return this.prisma.expense.update({
+      where,
+      data,
+    });
+  }
+
+  async create(data: Prisma.ExpenseCreateInput) {
+    return this.prisma.expense.create({
+      data,
+    });
+  }
+
+  async delete(where: Prisma.ExpenseWhereUniqueInput) {
+    return this.prisma.expense.delete({
+      where,
+    });
+  }
+}
