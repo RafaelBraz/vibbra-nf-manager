c7bd7dfe4fea85ab48dd7aad596fb1d54d84b73a
Rafael Braz <rafaelvcbraz@gmail.com>
2023-06-05 11:38:48 -0300

    Initial setup of next-auth (email & pass)

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,36 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { UserService } from "@/services/user/user.service";
+import { GetUserUseCase } from "@/use-cases/user/get.user.use-case";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    CredentialsProvider({
+      name: "Email e senha",
+      credentials: {
+        email: {
+          label: "E-mail:",
+          type: "email",
+          placeholder: "user@email.com",
+        },
+        password: { label: "Password:", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const userService = new UserService();
+        const getUserUseCase = new GetUserUseCase(userService);
+
+        if (!credentials?.email) return null;
+
+        const user = await getUserUseCase.execute({
+          where: {
+            email: credentials?.email,
+          },
+        });
+
+        return user;
+      },
+    }),
+  ],
+};
+export default NextAuth(authOptions);
